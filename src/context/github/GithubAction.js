76251fe5101d2779a00f0771e38762846cdb154e
hoangdigan <42528214+hoangdigan@users.noreply.github.com>
2022-10-04 20:24:13 +0700

    Update GithubAction.js
    x

--- src/context/github/GithubAction.js
+++ src/context/github/GithubAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GITHUB_URL = "https://api.github.com"
+const GITHUB_TOKEN = "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"
 
 
 const github = axios.create({
